cb9c6985739ac237cf577608baf7dea9b1a3643a
ChuuHsiang Hung <eric.hung0404@gmail.com>
2019-12-07 17:30:20 -0500

    Find shortest path

--- server.py
+++ server.py
@@ -1,44 +1,64 @@
 import googlemaps
 import requests
-from flask import Flask, render_template, request
 import os
+import osmnx as ox
+import numpy as np
+import networkx as nx
+import geopy.distance
+
+from flask import Flask, render_template, request, jsonify
 from config import GOOGLEMAP_API_KEY
 
-gmaps_key =googlemaps.Client(key = GOOGLEMAP_API_KEY) #google api key for using google services
+googlemap_cli =googlemaps.Client(key = GOOGLEMAP_API_KEY) #google api key for using google services
 
 app = Flask(__name__) #flask
 
-@app.route('/search')  #flask, suppose we have url like http://127.0.0.1:5000/search?start=A&destination=B
+@app.route('/search')  #flask, suppose we have url like http://127.0.0.1:5000/search?origin=A&destination=B
 def search():
-    start = request.args.get('start', type=str)
-    destination = request.args.get('destination', type=str)
-    return start       #it should return A
+    origin = request.args.get('origin', type=str)
+    dest = request.args.get('destination', type=str)
 
-address_list = ['50 Meadow st, amherst, MA', '9 Midvale Ave, '
-                'Jefferson, NY', '18 Hampten St, Worcester, MA'] #this is the example address list
-geocode_list = []
+    geocode_list = address_to_geocode([origin, dest])
+
+    geo_origin = geocode_list[0]
+    geo_dest = geocode_list[1]
+    dist = geopy.distance.distance(geo_origin, geo_dest).m
+
+    print(type(geo_origin))
+
+    G = ox.graph_from_point(geo_origin, distance=dist, network_type='walk')
+
+    ox.elevation.add_node_elevations(G, GOOGLEMAP_API_KEY, max_locations_per_batch=350, pause_duration=0.02)
+    ox.elevation.add_edge_grades(G, add_absolute=True)
+
+    nearest_origin = ox.get_nearest_node(G, geo_origin)
+    nearest_dest = ox.get_nearest_node(G, geo_dest)
+
+    route = nx.shortest_path(G, nearest_origin, nearest_dest)
+    
+    waypoints = getWaypoints(G, route)
+
+    return jsonify(waypoints)       #it should return A
+    return jsonify('start')
+
 def address_to_geocode(address_list): #find the geocode by address
-    for address in address_list:
-        geocode = {}
-        geocode_result = gmaps_key.geocode(address)
+    geocode_list = []
+    for address in address_list: 
+        geocode = []
+        geocode_result = googlemap_cli.geocode(address)
         lat = geocode_result[0]['geometry']['location']['lat']
         lng = geocode_result[0]['geometry']['location']['lng']
-        geocode['lat'] = lat
-        geocode['lng'] = lng
+        geocode.append(lat)
+        geocode.append(lng)
         geocode_list.append(geocode)
-        print(geocode_list)
     return geocode_list
 
-def get_altitude(geocode_list): #find altitude data by geocode, since they are not together.
-    for dict in geocode_list:
-        lat = dict['lat']
-        lng = dict['lng']
-        query = f'https://maps.googleapis.com/maps/api/elevation/json?locations={lat},{lng}&key=' + api_key
-        request = requests.get(query).json()
-        alt = request['results'][0]['elevation']
-        dict['alt'] = alt
-        print(geocode_list)
-    return geocode_list
+def getWaypoints(G, route): 
+    waypoints = []
+    for node in route: 
+        point = G.nodes[node]
+        waypoints.append({'x': point.get('x'), 'y':point.get('y')})
+    return waypoints
 
 if __name__ == "__main__": #flask
     app.run(debug=True)
